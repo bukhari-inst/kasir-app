7a953d8875072f5930d1ae9655bee870756b845e
Bukhori Inst <58775021+bukhari-inst@users.noreply.github.com>
2021-09-08 11:32:12 +0700

    setting font, desain layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
-import NavbarComponent from 'components/NavbarComponent';
+import { Row, Col, Container } from 'react-bootstrap';
+import { NavbarComponent, ListCategories, Hasil } from 'components';
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
+      <div className="mt-2">
+        <Container fluid>
+          <Row>
+            <ListCategories />
+            <Col>
+              <h4>
+                <strong>Daftar Produk</strong>
+              </h4>
+            </Col>
+            <Hasil />
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import NavbarComponent from 'components/NavbarComponent';
+import ListCategories from 'components/ListCategories';
+import Hasil from 'components/Hasil';
+
+export { NavbarComponent, ListCategories, Hasil };
