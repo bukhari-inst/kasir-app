ea278385a6df42ab12d9f1b83dcddca23a024e72
Bukhori Inst <58775021+bukhari-inst@users.noreply.github.com>
2021-09-08 12:49:40 +0700

    menampilkan menu produk

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import './App.css';
+import React, { Component } from 'react';
 import { Row, Col, Container } from 'react-bootstrap';
-import { NavbarComponent, ListCategories, Hasil } from 'components';
+import { NavbarComponent, ListCategories, Hasil, Menus } from 'components';
+import { API_URL } from 'utils/constanst';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <NavbarComponent />
-      <div className="mt-2">
-        <Container fluid>
-          <Row>
-            <ListCategories />
-            <Col>
-              <h4>
-                <strong>Daftar Produk</strong>
-              </h4>
-            </Col>
-            <Hasil />
-          </Row>
-        </Container>
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menus: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_URL + 'products')
+      .then((res) => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    const { menus } = this.state;
+
+    return (
+      <div className="App">
+        <NavbarComponent />
+        <div className="mt-2">
+          <Container fluid>
+            <Row>
+              <ListCategories />
+              <Col>
+                <h4>
+                  <strong>Daftar Produk</strong>
+                </h4>
+                <hr />
+                <Row>
+                  {menus &&
+                    menus.map((menu) => <Menus key={menu.id} menu={menu} />)}
+                </Row>
+              </Col>
+              <Hasil />
+            </Row>
+          </Container>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import NavbarComponent from 'components/NavbarComponent';
 import ListCategories from 'components/ListCategories';
 import Hasil from 'components/Hasil';
+import Menus from 'components/Menus';
 
-export { NavbarComponent, ListCategories, Hasil };
+export { NavbarComponent, ListCategories, Hasil, Menus };
